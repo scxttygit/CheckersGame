7bf16a0ceb6f1e429d03de00a85666ced71cd9ec
scxttygit <160169178+scxttygit@users.noreply.github.com>
2024-10-13 22:25:04 -0400

    Update utilities.h

--- utilities.h
+++ utilities.h
@@ -1,12 +1,10 @@
-
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
 #include <stdint.h>
 
-// Function prototypes
 int movePiece(uint64_t* playerPieces, uint64_t* opponentPieces, int startRow, int startCol, int endRow, int endCol);
-void promoteKing(uint64_t* playerPieces, uint64_t* playerKings, int endRow, int player);
+void promoteKing(uint64_t* playerPieces, uint64_t* playerKings, int endRow, int endCol, int player);
 int isCaptureMove(uint64_t playerPieces, uint64_t opponentPieces, int startRow, int startCol, int endRow, int endCol);
 int capturePiece(uint64_t* playerPieces, uint64_t* opponentPieces, int startRow, int startCol, int endRow, int endCol);
 
